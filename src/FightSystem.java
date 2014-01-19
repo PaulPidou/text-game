4ca77148bb11356f2a16b5d79d20ca2f18a99056
Paul Pidou <pidou.paul@gmail.com>
2014-01-19 21:50:23 +0100

    Upgrade fight system

--- src/FightSystem.java
+++ src/FightSystem.java
@@ -1,3 +1,15 @@
 public class FightSystem {
-  
+	
+	public Alive Fight(Fighter fighter1, Fighter fighter2) {
+		Alive winner = null;
+		
+		while(fighter1.isAlive() && fighter2.isAlive()) {
+			fighter1.hit(fighter2);
+			if(fighter2.isAlive())
+				fighter2.hit(fighter1);
+			
+		}
+		
+		return winner;
+	}
 }
