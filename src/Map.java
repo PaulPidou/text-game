5cd54dee806e2745d63c615455852f3522b1d645
Tristan Klempka <klempka.tristan@gmail.com>
2014-02-05 11:00:56 +0100

    Update Map.java

--- src/Map.java
+++ src/Map.java
@@ -2,7 +2,7 @@ public class Map {
 
 	  private ArrayList<Displayable> list = new ArrayList<Displayable>();
 	  private MapLoader;
-	  private MapConstructor;
+	  private MapBuilder;
 
 	  Map(String seed) {} 
 	  
