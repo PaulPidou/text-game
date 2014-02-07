349d347de58e1b9b4869ce0e6f7cccac2a5c15e2
PaulPidou <pidou.paul@gmail.com>
2014-02-07 13:46:16 +1030

    Update Action.java

--- src/Action.java
+++ src/Action.java
@@ -5,28 +5,15 @@ import java.util.ArrayList;
  */
 public class Action {
     
-	public void pickUp(String name, Game game) {
+    public static void pickUp(String name, Game game) {
     	Room tempRoom = game.getCurrentRoom(game.getHero());
     	Item tempItem = tempRoom.getByName(name);
     	boolean place = game.getHero().addItem(tempItem);
     	if(place) // If the inventory of the Hero is already full
     		tempRoom.removeItem(tempItem);
     }
-	
-    public void pickUpWeapon(String name, Game game) {
-    	Room tempRoom = game.getCurrentRoom(game.getHero());
-    	Item tempItem = tempRoom.getByName(name);
-    	if(tempItem instanceof Weapon) {
-    		Weapon tempWeapon = game.getHero().getWeapon();
-    		game.getHero().chooseWeapon((Weapon)tempItem);
-    		tempRoom.removeItem(tempItem);
-    		tempRoom.addItem(tempWeapon);
-    	}
-    	else
-    		System.out.println("It is not a Weapon");	
-    }
     
-    public void dropItem(String name, Game game) {
+    public static void dropItem(String name, Game game) {
     	Room tempRoom = game.getCurrentRoom(game.getHero());
     	Item tempItem = game.getHero().getInventory().getByName(name);
     	tempRoom.addItem(tempItem);
